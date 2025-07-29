d064e0c0176051306b240807a09bd812de69e6cc
TeckelFou <laurine.groven@gmail.com>
2025-07-29 16:43:07 +0200

    S004

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 
 const express = require('express');
 const app = express();
+
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+//ca sert à utiliser le dossier public
+
 app.set("views", "./views"); 
 app.set("view engine", "ejs");
 
-let message = "help oui !!";
+let message = "Tickets";
 
 app.get("/",(req,res)=> {
-    res.render("home", {message: message });
+    res.render("liste-tickets", {message: message });
 });
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
